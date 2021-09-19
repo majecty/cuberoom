ef2d9c6192029f43142c6c557bf5148f94fd2ade
Park Juhyung(Borre) <jh@majecty.com>
2021-09-20 03:25:02 +0900

    ✨ make client easy to run in local (#10)

---
+++ client/ENV.js
@@ -0,0 +1,11 @@
+const ENV = {
+  // development
+  URL: "http://localhost:3000",
+  URL_STATIC: "http://localhost:3000/static",
+  ENVIRONMENT: "development",
+  // production
+  // URL: 'http://cuberoom.net',
+  // URL_STATIC: 'http://cuberoom.net',
+  // ENVIRONMENT: 'production',
+};
+export default ENV;
---
+++ client/README.md
@@ -0,0 +1,13 @@
+# Cuberoom client
+
+## 프로젝트 셋업하기
+
+```sh
+yarn
+```
+
+## 로컬에서 실행하기
+
+```sh
+yarn run dev
+```

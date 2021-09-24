532221de24568804e73ebff0e384423bc9d9f754
ParkJuhyung <majecty@gmail.com>
2021-09-24 11:04:51 +0900

    🔧 add "/" prefix to the user image prefix

--- server/__init__.py
+++ server/__init__.py
@@ -27,7 +27,7 @@ config_values["production"] = {
   "static_folder": "static",
   "cors_origin": "http://cuberoom.net",
   "public_path": "../cuberoom/public", # please check this in the deployed environment
-  "user_image_prefix": "results",
+  "user_image_prefix": "/results",
   "port": 5000 # default port in flask
 }
 

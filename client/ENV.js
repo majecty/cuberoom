0157e4d6b235ae41a1c219347f470aee4daa1dd8
ParkJuhyung <majecty@gmail.com>
2021-09-26 13:44:38 +0900

    📈 report error to sentry

--- client/ENV.js
+++ client/ENV.js
@@ -2,15 +2,17 @@ const devEnv = {
   URL: "http://localhost:3000",
   URL_STATIC: "http://localhost:3000/static",
   ENVIRONMENT: "development",
+  tracesSampleRate: 1.0,
 };
 
 const productionEnv = {
   URL: "http://test.cuberoom.net",
   URL_STATIC: "http://test.cuberoom.net",
   ENVIRONMENT: "production",
+  tracesSampleRate: 0.1,
 };
 
-let ENV;
+let ENV = devEnv;
 if (process.env.BUILD_ENV === "production") {
   ENV = productionEnv;
 } else {
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,21 @@
 import App from "./App.svelte";
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
+import ENV from "../ENV";
 
 const app = new App({
   target: document.body,
   // hydrate: true,
+});
+
+Sentry.init({
+  dsn: "https://21f1b2ad5efb452684d66b18467ae893@o1013913.ingest.sentry.io/5979255",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: ENV.tracesSampleRate,
 });
 
 export default app;

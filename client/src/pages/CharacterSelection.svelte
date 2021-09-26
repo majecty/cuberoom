bccc38692d0369b88f36b9bb4f58c4ac77c8312d
ParkJuhyung <majecty@gmail.com>
2021-09-26 21:18:22 +0900

    ✨ save id and password in browser storage

--- client/src/pages/CharacterSelection.svelte
+++ client/src/pages/CharacterSelection.svelte
@@ -4,6 +4,12 @@
   import ENV from '../../ENV';
   import names from '../entity/names';
   import { saveToBrowserStorage, loadFromBrowserStorage } from "./storage";
+  import { uuidv4, randomPassword } from "../util/random";
+
+  const uniqueId = uuidv4();
+  const password = randomPassword();
+  saveToBrowserStorage("id", uniqueId);
+  saveToBrowserStorage("password", password);
 
   let skinNum = 1;
   let eyeNum = 1;
---
+++ client/src/util/random.js
@@ -0,0 +1,20 @@
+// copied from https://stackoverflow.com/a/2117523
+export function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+// copied from https://w3collective.com/random-password-generator-javascript/
+export function randomPassword() {
+  const length = 16
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return password;
+}

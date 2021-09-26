aab6dc62d282709927b00a032fd4df244ba1bcc1
ParkJuhyung <majecty@gmail.com>
2021-09-26 13:36:48 +0900

    🔧 automatically use production env

--- client/ENV.js
+++ client/ENV.js
@@ -1,11 +1,20 @@
-const ENV = {
-  // development
+const devEnv = {
   URL: "http://localhost:3000",
   URL_STATIC: "http://localhost:3000/static",
   ENVIRONMENT: "development",
-  // production
-  // URL: 'http://test.cuberoom.net',
-  // URL_STATIC: 'http://test.cuberoom.net',
-  // ENVIRONMENT: 'production',
 };
+
+const productionEnv = {
+  URL: "http://test.cuberoom.net",
+  URL_STATIC: "http://test.cuberoom.net",
+  ENVIRONMENT: "production",
+};
+
+let ENV;
+if (process.env.BUILD_ENV === "production") {
+  ENV = productionEnv;
+} else {
+  ENV = devEnv;
+}
+
 export default ENV;

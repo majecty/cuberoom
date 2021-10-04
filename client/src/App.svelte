1b4016416cc0241ff4abf0759f56991ab2d08f51
Park Juhyung(Borre) <jh@majecty.com>
2021-10-04 10:40:10 +0900

    🐛 fix issues related to mobile (#30)
    * 🚸 disable zoom
    * 🐛 do not stop on elevator in mobile

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -4,6 +4,34 @@
 	import CharacterSelection from './pages/CharacterSelection.svelte';
 	import Map from './pages/Map.svelte';
 	import Game from './pages/Game.svelte';
+
+
+  if (window.disableDoubleTapToZoom !== true) {
+    var elm = document.body; // or some selection of the element you want to disable
+    var doubleTouchStartTimestamp = 0;
+    var catcher = function(evt) {
+        var now = +(new Date());
+        if (doubleTouchStartTimestamp + 1000 > now) {
+            event.preventDefault();
+        }
+        doubleTouchStartTimestamp = now;
+    };
+    elm.addEventListener('touchstart', catcher, true);
+
+
+    let last_touch_end = 0;
+    document.addEventListener("touchend", function (e) {
+      const now = (new Date()).getTime();
+      if (now - last_touch_end <= 300) {
+          e.preventDefault();
+      }
+      last_touch_end = now;
+    }, false);
+
+    window.disableDoubleTapToZoom = true;
+  }
+
+
 </script>
 
 <Router>

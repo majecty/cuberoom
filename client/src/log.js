77ce628bcbfefd066fb687a91f66021873c25a15
Park Juhyung <majecty@gmail.com>
2021-10-17 23:01:32 +0900

    🔊 reduce too many error logs

--- client/src/log.js
+++ client/src/log.js
@@ -1,4 +1,5 @@
 import { ifDebug } from "./common/debug";
+import { rateLimiterCreate, rateLimiterTrigger } from "./network/rateLimiter";
 
 /* eslint-disable import/prefer-default-export */
 
@@ -7,8 +8,30 @@ export function log(...args) {
   console.log(...args);
 }
 
+const errorRateLimiter = rateLimiterCreate();
+const first5Errors = [];
+const last5Errors = [];
+const ignoredErrorCount = 0;
+
+window.errors = {
+  first5Errors,
+  last5Errors,
+  getIgnoredErrorCount: () => ignoredErrorCount,
+};
+
 export function logErr(...args) {
-  console.error(...args);
+  if (first5Errors.length < 5) {
+    first5Errors.push([...args]);
+  }
+  last5Errors.push([...args]);
+  if (last5Errors.length > 5) {
+    last5Errors.shift();
+  }
+  ignoredErrorCount += 0;
+  rateLimiterTrigger(errorRateLimiter, () => {
+    console.error(...args);
+    ignoredErrorCount -= 0;
+  });
 }
 
 export function logDebug(...args) {

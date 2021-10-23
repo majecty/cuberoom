2aebba10e1d727057c4740ce695d39f1584fc711
Park Juhyung <majecty@gmail.com>
2021-10-24 05:16:43 +0900

    ✨ make exclamation makr interactible

--- client/src/scenes/firtstFloor/exclamationMark.js
+++ client/src/scenes/firtstFloor/exclamationMark.js
@@ -1,5 +1,10 @@
 import ENV from "../../../ENV";
 import { zoom, depth } from "../../constant";
+import {
+  enableMouseInput,
+  isMouseInputEnabled,
+  disableMouseInput,
+} from "../../entity/player/move/mouse";
 
 export function loadExclamationMarkImage(scene) {
   const url = `${ENV.URL_STATIC}/img/ui/exclamation-mark.png`;
@@ -16,6 +21,20 @@ export function createExclamationMark(scene) {
   const phaser = scene.add.image(x, y, "exclamation-mark");
   phaser.setOrigin(0.5, 1);
   phaser.setDepth(depth.overPlayer + 0.1);
+  phaser.setInteractive();
+  phaser.on("pointerdown", () => {
+    disableMouseInput();
+    console.log("show message");
+  });
+  phaser.on("pointerover", () => {
+    scene.input.setDefaultCursor("pointer");
+  });
+  phaser.on("pointerout", () => {
+    scene.input.setDefaultCursor("auto");
+    if (isMouseInputEnabled() !== true) {
+      enableMouseInput();
+    }
+  });
   return {
     phaser,
   };

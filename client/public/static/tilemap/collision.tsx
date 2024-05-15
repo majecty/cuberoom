e96708fe28f5f019681021afc3bcac4cfb09185d
Park Juhyung <me@juhyung.dev>
2024-05-15 19:25:06 +0900

    tilemap 파일 추가

---
+++ client/public/static/tilemap/collision.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="collision" tilewidth="16" tileheight="16" tilecount="4" columns="2">
+ <image source="simple_tile.png" width="32" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="art1"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ client/public/static/tilemap/interactive-tile.tsx
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="interactive-tile" tilewidth="16" tileheight="16" tilecount="24" columns="6">
+ <image source="interactive-tile.png" width="96" height="64"/>
+ <tile id="0" type="image1">
+  <properties>
+   <property name="name" value="up"/>
+  </properties>
+ </tile>
+ <tile id="1" type="image2">
+  <properties>
+   <property name="name" value="down"/>
+  </properties>
+ </tile>
+ <tile id="2" type="image3">
+  <properties>
+   <property name="name" value="elevator"/>
+  </properties>
+ </tile>
+ <tile id="3" type="image4">
+  <properties>
+   <property name="name" value="out"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="name" value="image2-popup"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="name" value="image2-description"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="name" value="image3"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="name" value="image4"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="name" value="image3-popup"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="name" value="image3-description"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="name" value="image4-popup"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="name" value="image4-description"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="name" value="image5"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="name" value="image6"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="name" value="image9"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="name" value="image5-description"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="name" value="image6-description"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="name" value="image9-description"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="name" value="image7"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="name" value="image8"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="name" value="image"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="name" value="image7-description"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="name" value="image8-description"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="name" value="image"/>
+  </properties>
+ </tile>
+</tileset>

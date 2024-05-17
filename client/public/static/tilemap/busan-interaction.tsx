5095ce6ed219977df950cc28ea4740301484a8bc
Park Juhyung <me@juhyung.dev>
2024-05-17 22:42:54 +0900

    타일맵 추가

---
+++ client/public/static/tilemap/busan-interaction.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="busan-interaction" tilewidth="16" tileheight="16" tilecount="40" columns="10">
+ <image source="busan-interactive.png" width="160" height="64"/>
+ <tile id="2">
+  <properties>
+   <property name="name" value="toRoof"/>
+  </properties>
+ </tile>
+</tileset>

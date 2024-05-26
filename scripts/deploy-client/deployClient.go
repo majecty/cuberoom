80e2de24ef8f99d1896fd51077e584947a5721c7
juhyung <me@juhyung.dev>
2024-05-26 16:42:16 +0900

    aws profile 체크하는 코드를 deploy script에 추가

--- scripts/deploy-client/deployClient.go
+++ scripts/deploy-client/deployClient.go
@@ -10,16 +10,20 @@ import (
 	"github.com/manifoldco/promptui"
 )
 
+const (
+	awsProfile = "juhyung"
+)
+
 func main() {
 	ensureCuberoomRoot()
 	ensureAWSCLI()
 	runAWSS3Sync()
 
-	fmt.Println("Hello, World!")
+	fmt.Println("Done!")
 }
 
 func runAWSS3Sync() {
-	command := "aws s3 sync client/public s3://cuberoom"
+	command := fmt.Sprintf("aws s3 sync client/public s3://cuberoom --profile %s", awsProfile)
 	prmpt := promptui.Prompt{
 		Label:     "다음 명령어를 실행할까요? " + command,
 		IsConfirm: true,
@@ -46,6 +50,36 @@ func runAWSS3Sync() {
 
 func ensureAWSCLI() {
 	log.Printf("NOT IMPLEMENTED: Checking for AWS CLI")
+	cmd := "aws --version"
+	fmt.Printf("Running command: %s\n", cmd)
+	_, err := script.Exec("aws --version").Stdout()
+	if err != nil {
+		panic(fmt.Errorf("aws cli (%s) failed %w", cmd, err))
+	}
+
+	getProfile := fmt.Sprintf("aws configure get aws_access_key_id --profile %s", awsProfile)
+	fmt.Printf("Running command: %s\n", getProfile)
+	if _, err := script.Exec(getProfile).WithStderr(os.Stderr).Stdout(); err != nil {
+		panic(fmt.Errorf("aws cli (%s) failed %w", getProfile, err))
+	}
+
+	callerIdentity := fmt.Sprintf("aws sts get-caller-identity --profile %s", awsProfile)
+	fmt.Printf("Running command: %s\n", callerIdentity)
+	if _, err := script.Exec(callerIdentity).Stdout(); err != nil {
+		panic(fmt.Errorf("aws cli (%s) failed %w", callerIdentity, err))
+	}
+
+	prmpt := promptui.Prompt{
+		Label:     "AWS CLI 확인 완료?",
+		IsConfirm: true,
+	}
+	if _, err := prmpt.Run(); err != nil {
+		if errors.Is(promptui.ErrAbort, err) || errors.Is(promptui.ErrInterrupt, err) {
+			fmt.Println("취소되었습니다.")
+			os.Exit(0)
+		}
+		panic(fmt.Errorf("prompt failed %w", err))
+	}
 }
 
 func ensureCuberoomRoot() {

966eb90a2708fcbc674d1c22bd3f73a53dd499e2
juhyung <me@juhyung.dev>
2024-05-26 15:49:47 +0900

    s3 sync 를 실행하는 고 스크립트 작성

--- scripts/deploy-client/deployClient.go
+++ scripts/deploy-client/deployClient.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/bitfield/script"
 )
 
 func main() {
 	ensureCuberoomRoot()
+	ensureAWSCLI()
+	runAWSS3Sync()
+
 	fmt.Println("Hello, World!")
+}
+
+func runAWSS3Sync() {
+	command := "aws s3 sync client/public s3://cuberoom"
+	// TODO: confirm from user
+	_, err := script.Exec(command).Stdout()
+	if err != nil {
+		panic(fmt.Errorf("error syncing to s3 %w", err))
+	}
+}
+
+func ensureAWSCLI() {
+	log.Printf("NOT IMPLEMENTED: Checking for AWS CLI")
 }
 
 func ensureCuberoomRoot() {

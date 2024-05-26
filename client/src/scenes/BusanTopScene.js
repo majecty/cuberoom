34f070fcce58b21870d506c4d4ee0a11023d8d70
juhyung <me@juhyung.dev>
2024-05-26 23:23:04 +0900

    부산 옥상 맵 업데이트

--- client/src/scenes/BusanTopScene.js
+++ client/src/scenes/BusanTopScene.js
@@ -8,8 +8,8 @@ import { baseSceneConstructor, baseSceneCreate, baseSceneInit, baseScenePreload,
 
 function backgroundStatic(scene) {
   const sprite = scene.add.sprite(
-    320 / zoom,
-    480 / zoom,
+    464 / zoom,
+    456 / zoom,
     "busanTop-background"
   );
   sprite.scale = 2 / zoom;
